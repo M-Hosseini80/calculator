474b15d4e21b7ef2fef5d96e9cc31e013e8e79f4
seyed <M_Hosseini80@yahoo.com>
2021-09-29 09:26:19 +0330

    change calculator method - fixed calculator operators bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Button from './components/Button'
 import Input from './components/Input'
 import React, { useState } from 'react';
+import './App.css'
 // import { Children } from 'react';
 // import reactDom from 'react-dom';
 function App() {
   const [val, setVal] = useState(0);
+  // var result = false;
+
   var last_char = val[val.length - 1];
   // var new_val = val;
   if (last_char === '/' || last_char === '*' || last_char === '-' || last_char === '+' || last_char === '=') {
@@ -14,8 +17,10 @@ function App() {
     last_char = false;
   }
   function ButtonClk(e) {
+
     return (
-      setVal(val + "" + e.target.innerText)
+
+      val === 0 ? setVal(e.target.innerText) : setVal(val + "" + e.target.innerText)
       // console.log(val[val.length - 1])
       // console.log(e.target.innerText + val)
       // document.querySelector('.buttons__si').target.disabled = true
@@ -53,15 +58,20 @@ function App() {
     // }
     // console.log(total)
 
-    function addbits(sumTotal) {
-      var total = 0,
-        sumTotalVal = sumTotal.match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
+    // function addbits(sumTotal) {
+    //   var total = 0,
+    //     sumTotalVal = sumTotal.match(/[+-]*(\.\d+|\d+(\.\d+)?)/g) || [];
 
-      while (sumTotalVal.length) {
-        total += parseFloat(sumTotalVal.shift());
-      }
-      return total;
-    }
+    //   while (sumTotalVal.length) {
+    //     total += parseFloat(sumTotalVal.shift());
+    //   }
+    //   return total;
+    // }
+
+
+    // function evil(fn) {
+    //   return (new Function('return ' + fn)());
+    // }
 
     // var string = '1+23+4+5-30';
     // console.log(
@@ -74,18 +84,40 @@ function App() {
     // total.parse();
     // total = total.evaluate();
     // console.log(Vtotal)
-    return (
-      !last_char ? setVal(val + "" + e.target.innerText + " " + addbits(val)) : setVal(val.slice(0, -1) + e.target.innerText)
+    var result;
+    var status = false;
+    try {
+      if (val.length) { result = eval(val); status = true }
 
+    }
+    catch (ex) {
+      console.log(ex)
+    }
+    // finally {
+    //   result = setVal(0)
+    // }
+
+    return (
+
+      status ? !last_char ? setVal(val + "" + e.target.innerText + result) : setVal(0) : setVal(0)
+      // !result ? !last_char ? setVal(val + "" + e.target.innerText + eval(val)) : setVal(0) : setVal(0),
+      // last_char ?? result === true
       // console.log(e)
       // e.target.disabled = 'true'
     )
+
+  }
+  /* eslint no-eval: 0 */
+  function Clear() {
+    setVal(0)
   }
   return (
-    <div className="App">
+    <div className="Calculator">
       <Input value={val} /><br />
 
+      <Button onClick={Clear}>Clear</Button>
       <Button onClick={ButtonClk}>0</Button>
+      <Button onClick={ButtonClk}>.</Button>
       <Button onClick={ButtonClk}>1</Button>
       <Button onClick={ButtonClk}>2</Button>
       <Button onClick={ButtonClk}>3</Button>
